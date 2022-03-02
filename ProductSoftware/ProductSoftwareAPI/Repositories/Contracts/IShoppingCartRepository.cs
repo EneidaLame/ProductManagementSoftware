cf78b402f5ab306990a524e860fc9ee5e81b19aa
eneidalame <eneida_lame@hotmail.com>
2022-03-02 21:10:31 +0200

    CartItem

---
+++ ProductSoftware/ProductSoftwareAPI/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductSoftwareAPI.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ProductSoftwareAPI.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto);
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto);
+        Task<CartItem> DeleteItem(int id);
+        Task<CartItem> GetItem(int id);
+        Task<IEnumerable<CartItem>> GetItems(int userId);
+
+
+    }
+}
